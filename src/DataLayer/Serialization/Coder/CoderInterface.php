bdc36474648a29d4161bf4b466f0b050295eb708
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-03-12 21:08:56 +0300

    DataLayer\Serialization\Coder\EloquentModelCoder work in progress + some refactorings

--- src/DataLayer/Serialization/Coder/CoderInterface.php
+++ src/DataLayer/Serialization/Coder/CoderInterface.php
@@ -2,9 +2,24 @@
 
 namespace FHTeam\LaravelRedisCache\DataLayer\Serialization\Coder;
 
+/**
+ * Interface CoderInterface
+ *
+ * @package FHTeam\LaravelRedisCache\DataLayer\Serialization\Coder
+ */
 interface CoderInterface
 {
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
     public function decode($value);
 
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
     public function encode($value);
 }
--- src/DataLayer/Serialization/Coder/EloquentModelCoder.php
+++ src/DataLayer/Serialization/Coder/EloquentModelCoder.php
@@ -2,6 +2,10 @@
 
 namespace FHTeam\LaravelRedisCache\DataLayer\Serialization\Coder;
 
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 /**
  * Class responsible for encoding / decoding Eloquent models
  *
@@ -10,32 +14,105 @@ namespace FHTeam\LaravelRedisCache\DataLayer\Serialization\Coder;
 class EloquentModelCoder implements CoderInterface
 {
 
+    const TYPE_MODEL = 'model';
+    const TYPE_COLLECTION = 'collection';
+
     public function decode($value)
     {
-        // TODO: Implement decode() method.
+        if (!isset($value['type'])) {
+            throw new Exception("Attempt to deserialize damaged object (no 'type' data): " . json_encode($value));
+        }
+
+        switch ($value['type']) {
+            case self::TYPE_MODEL:
+                return $this->decodeModel($value);
+                break;
+            case self::TYPE_COLLECTION:
+                return $this->decodeCollection($value);
+                break;
+            default:
+                throw new Exception("Unable to deserialize type '{$value['type']}'");
+        }
     }
 
     public function encode($value)
     {
-        // TODO: Implement encode() method.
+        if ($value instanceof Collection) {
+            return $this->encodeCollection($value);
+        }
+
+        if ($value instanceof Model) {
+            return $this->encodeModel($value);
+        }
+
+        throw new Exception("Unable to serialize data of class " . get_class($value));
     }
 
-    protected function encodeModel()
+    /**
+     * @param Model $model
+     *
+     * @return array
+     * @throws Exception
+     */
+    protected function encodeModel(Model $model)
     {
+        $result = [
+            'type' => self::TYPE_MODEL,
+            'class' => get_class($model),
+            'attributes' => $model->getAttributes(),
+        ];
 
+        foreach ($model->getRelations() as $relationName => $relationData) {
+            if ($relationData instanceof Collection) {
+                $result['relations'][$relationName] = $this->encodeCollection($relationData);
+                continue;
+            }
+
+            if ($relationData instanceof Model) {
+                $result['relations'][$relationName] = $this->encode($relationData);
+                continue;
+            }
+
+            throw new Exception("Unable to serialize data of class " . get_class($relationData));
+        }
+
+        return $result;
     }
 
-    protected function encodeCollection()
+    /**
+     * @param Collection $collection
+     *
+     * @return array
+     * @throws Exception
+     */
+    protected function encodeCollection(Collection $collection)
     {
+        $result = [
+            'type' => self::TYPE_COLLECTION,
+            'items' => [],
+        ];
+        foreach ($collection as $item) {
+            $result['items'][] = $this->encodeModel($item);
+        }
 
+        return $result;
     }
 
-    protected function decodeModel()
+    protected function decodeModel($value)
     {
     }
 
-    protected function decodeCollection()
+    protected function decodeCollection(array $value)
     {
+        if (!isset($value['items'])) {
+            throw new Exception("Attempt to deserialize damaged collection (no 'items' data): " . json_encode($value));
+        }
 
+        $result = [];
+        foreach ($value['items'] as $item) {
+            $result[] = $this->decodeModel($item);
+        }
+
+        return new \Illuminate\Database\Eloquent\Collection($result);
     }
 }
